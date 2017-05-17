1250001f4c799f7583a3f88c531b558ed0578fe2
Jvaneerd <J.vaneerd@student.fontys.nl>
2017-05-17 11:53:01 +0200

    Clean up directories

--- Code_Integration/Alarm_System_Snapshots/Integration_Snapshots/3_LED_Siren_Timer_PW/src/PWManager.cc
+++
@@ -1,9 +0,0 @@
-#include "PWManager.hh"
-
-PWManager::PWManager(const dzn::locator& loc) : skel::PWManager(loc) {
-  //no op
-}
-
-bool PWManager::iPWManager_verifyPassword(std::string pw) {
-  return pw.compare(this->password) == 0;
-}
